310d4658717b680badedb413358899cbbd9c9d52
StephAC9 <nnstephan9@yahoo.com>
2020-03-03 15:01:08 +0100

    Header, nav bar first step

---
+++ FrontEnd/retrofashion/src/views/orders/Payment1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Payment">
+    <h1>Payment1</h1>
+  </div>
+</template>
+<script>
+export default {
+    name:'Payment'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ FrontEnd/retrofashion/src/views/products/categories/ChildrenProducts.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Children</h1>
+    </div>
+</template>
