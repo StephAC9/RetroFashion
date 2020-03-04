f8eee90a8e6f92685d36591997985df66f208beb
Martin Jjooga <37835192+Jjoogam0001@users.noreply.github.com>
2020-03-04 22:38:10 +0100

    cart

--- FrontEnd/retrofashion/src/App.vue
+++ FrontEnd/retrofashion/src/App.vue
@@ -1,32 +1,45 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template> 
+    <div>
+  	     <navbar></navbar>
+  		    <products></products>
+  		    
+  		    <cart></cart>
+  		    
+  		    <toast :show="toast.show" :text="toast.text" @hide-toast="hideToast"></toast>
+  	 </div>  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+    import Navbar from './components/Navbar.vue'
+    import Products from './components/Products.vue'
+    import Cart from './components/Cart.vue'
+    import Toast from './components/Toast.vue'
+    
+  	  export default {
+  		     data(){
+  			        return{
+  			
+  			        };
+  		      },
+  		      components:{
+  			        Navbar,
+  			        Products,
+  			        Cart,
+  			        Toast
+  		      },
+  		      computed: {
+  		          toast() {
+  		              return this.$store.getters.toast;
+  		          }
+  		      },
+  		      methods: {
+  		          hideToast() {
+  		              this.$store.commit("hideToast");
+  		          }
+  		      }
+ 	    }   
+</script>
 
-#nav {
-  padding: 30px;
-}
+<style scoped>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- FrontEnd/retrofashion/src/main.js
+++ FrontEnd/retrofashion/src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { store } from './store/store'
 
-Vue.config.productionTip = false
 
 new Vue({
-  router,
+  el: '#app',
   store,
   render: h => h(App)
-}).$mount('#app')
+})
+   
