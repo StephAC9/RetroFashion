bfaef89d73bc9b61f21ee6bf29878877bddbffdf
Martin Jjooga <Martin Jjooga>
2020-03-13 19:42:33 +0100

    aSASS

---
+++ FrontEnd/retrofashion/src/products.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        productName: "1960s Leatherette Block Heel Kadee Boot",
+        productDescription:"Women leather boots",
+        productColor: "",
+        productSize: 37,
+        productPrice: 300,
+        quantity: 10,
+        id: 1,
+    },
+    {
+        productName: "1970s Suede Platform Sandal Wedge",
+        productDescription:"Summer shoe",
+        productColor: "",
+        productSize: 37,
+        productPrice: 1100,
+        quantity: 10,
+        id: 2,
+    },
+  
+  
+]
