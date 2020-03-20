9db743e4c4e526d6e353bef919e15d48e8a308a1
kubrom <kubrom.mulugheta@gmail.com>
2020-03-20 12:39:05 +0100

    cart view page

--- FrontEnd/retrofashion/src/components/cart/ProductCardItem.vue
+++ FrontEnd/retrofashion/src/components/cart/ProductCardItem.vue
@@ -1,12 +1,196 @@
 <template>
-  <div class="ProductCardItem">
+  <div class="cart-product-item">
+    <div class="item-image">
+      <img
+        style="width: 100%; height:100%;"
+        :src="require(`../../../../Images/${product.productImages[0]}`)"
+        alt="img"
+      />
+    </div>
+    <div class="item-title item-margin-top">
+      <p>{{product.productName}}</p>
+      <p>SIZE: {{product.productSize}}</p>
+    </div>
+    <div class="item-quantity item-margin-top">
+      <div style="width: 100px; margin: 0 auto;  margin-top: 20px;  ">
+        <span class="value" @click="increment">+</span>
+        <span class="value">{{count}}</span>
+        <span class="value" @click="decrement">-</span>
+      </div>
+    </div>
+    <div class="item-price item-margin-top">
+      <p class="item-price-2">{{ productSubtotal(product.productPrice, count) }} SEK</p>
+
+      <div class="item-remove">
+        <span>X</span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {
-    name:'ProductCardItem'
-}
-</script>
-<style scoped>
+import { mapGetters } from "vuex";
 
+export default {
+  name: "product-cart-item",
+  data() {
+    return {
+      product: null,
+      count: 0
+    };
+  },
+  computed: {
+    ...mapGetters("women", ["products"])
+  },
+  watch: {
+    products(value) {
+      if (value !== null || value !== undefined) {
+        this.product = this.products[0];
+        console.log(this.products);
+      }
+    }
+  },
+  created() {
+    console.log("created");
+    console.log(this.products);
+  },
+  mounted() {
+    console.log("mounted");
+    this.product = this.products[4];
+    console.log(this.product.productName);
+    console.log(this.product.productSize);
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    decrement() {
+      if (this.count === 0) return;
+      this.count -= 1;
+    },
+    productSubtotal(price, quantity) {
+      return price * quantity;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.item-margin-top {
+  margin-top: 50px;
+}
+.cart-product-item {
+  margin-top: 15px;
+  margin-bottom: 15px;
+  display: flex;
+
+  background: rgb(235, 231, 231);
+  padding: 4px;
+}
+.item-image {
+  flex-basis: 15%;
+  height: 180px;
+  width: 180px;
+  padding: 3px;
+  padding-right: 30px;
+}
+.item-title {
+  flex-basis: 30%;
+}
+.item-quantity {
+  flex-basis: 35%;
+  height: 100%;
+  justify-content: center;
+
+  display: flex;
+}
+.item-price {
+  flex-basis: 20%;
+  position: relative;
+}
+p {
+  font-size: 16px;
+}
+.btn-light1,
+.btn-light2,
+.value {
+  font-size: 18px;
+  flex-basis: 33%;
+  margin: 10px;
+}
+.value:focus {
+  outline: none;
+}
+.item-remove {
+  position: absolute;
+  left: 150px;
+  top: -40px;
+  font-weight: bold;
+}
+.item-price-2 {
+  margin-top: 18px;
+  font-size: 24px;
+}
+
+/* 
+
+
+.delete-btn {
+  border-radius: 50%;
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  background: white;
+  font-size: 15px;
+  font-weight: bold;
+  text-align: center;
+  font-size: 7px;
+  padding-top: 8px;
+  z-index: 2;
+  top: -12px;
+  right: 15px;
+}
+
+.cart-product-item {
+  display: flex;
+  height: 180px;
+  width: auto;
+  margin-top: 100px;
+  margin-bottom: 200px;
+  padding: 10px;
+}
+
+
+.item {
+  padding: 20px 30px;
+  height: 120px;
+  display: flex;
+}
+.sampleMini {
+  margin-top: 65px;
+  width: 83px;
+  padding-top: 8px;
+  text-align: center;
+  font-size: 16px;
+  color: #43484d;
+  font-weight: 300;
+}
+
+.title {
+  height: 60px;
+  border-bottom: 1px solid #e1e8ee;
+  padding: 20px 30px;
+  color: #5e6977;
+  font-size: 18px;
+  font-weight: 400;
+}
+button .fa fa-window-minimize sign {
+  margin-bottom: 25px;
+}
+.sign {
+  margin: 10px;
+}
+@media (max-width: 650px) {
+} */
 </style>
--- FrontEnd/retrofashion/src/components/shared/header/sub-header/TopBar.vue
+++ FrontEnd/retrofashion/src/components/shared/header/sub-header/TopBar.vue
@@ -3,196 +3,202 @@
     <div class="navbar">
       <div class="top-bar">
         <div class="bar-left">
-          <b-navbar-brand style="background: none; font-family:cursive, sans-serif;text-decoration: none;"
-            class="logo a" href="/home">
-            <h1 style="display:inline">R</h1> etroFashion
+          <b-navbar-brand
+            style="background: none; font-family:cursive, sans-serif;text-decoration: none;"
+            class="logo a"
+            href="/home"
+          >
+            <h1 style="display:inline">R</h1>etroFashion
           </b-navbar-brand>
         </div>
         <div class="bar-center">
           <form class="example" action="#">
-            <input type="text" placeholder="Search.." name="search">
-            <button class="btn" type="submit"><i class="fa fa-search"></i></button>
+            <input type="text" placeholder="Search.." name="search" />
+            <button class="btn" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
           </form>
         </div>
         <div class="bar-right">
           <div class="bar-right-content">
             <div class="nav-icon">
               <button class="btn-icon" :id="`popover-2-${placement}`">
-                <i class='fa fa-user _icon' style="margin-left:2px"></i>
+                <i class="fa fa-user _icon" style="margin-left:2px"></i>
               </button>
-              <popover layout='login'> </popover>
+              <popover layout="login"></popover>
               <div class="desciption">Login</div>
             </div>
             <div class="nav-icon favorites">
               <button class="btn-icon">
-                <i class='fa fa-gratipay _icon' style="margin-left:5px"></i>
+                <i class="fa fa-gratipay _icon" style="margin-left:5px"></i>
               </button>
               <div class="desciption">Favorites</div>
             </div>
-            <div class="nav-icon">
+            <div @click="goToCart" class="nav-icon">
               <button class="btn-icon" :id="`popover-1-${placement}`">
-                <i class='fa fa-cart-arrow-down _icon' style="margin-right:5px"></i>
+                <i class="fa fa-cart-arrow-down _icon" style="margin-right:5px"></i>
               </button>
-              <popover layout='cart'> </popover>
+              <popover layout="cart"></popover>
               <div style="width: 100%;margin-left:6px; text-align:start" class="desciption">Cart</div>
               <div v-show="items" class="items-count-notification">
-                <p><strong>100</strong></p>
+                <p>
+                  <strong>100</strong>
+                </p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  import Popover from '../../../popover/Popover'
-  export default {
-    name: 'top-bar',
-    components: {
-      Popover
-    },
-    data: () => ({
-      items: true,
-      placement: 'bottomleft',
-    }),
+import Popover from "../../../popover/Popover";
+export default {
+  name: "top-bar",
+  components: {
+    Popover
+  },
+  data: () => ({
+    items: true,
+    placement: "bottomleft"
+  }),
+  methods: {
+    goToCart() {
+      console.log("go to Cart");
+      this.$router.push("/cart");
+    }
   }
+};
 </script>
 <style scoped>
-  @import "../../../../assets/css/header-style.css";
+@import "../../../../assets/css/header-style.css";
 
-  a:hover {
-    transition: .5s;
-    text-decoration: none;
-    color: whitesmoke;
-    border-bottom: 3px solid white;
+a:hover {
+  transition: 0.5s;
+  text-decoration: none;
+  color: whitesmoke;
+  border-bottom: 3px solid white;
+}
+
+a {
+  font-size: 30px;
+}
+
+.bar-right-content {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+
+.nav-icon {
+  flex-basis: 15%;
+  text-align: end;
+  padding-top: 10px;
+  max-width: 50px;
+  position: relative;
+  z-index: 0;
+}
+
+.desciption {
+  color: white;
+  font-size: 10px;
+  z-index: 2;
+  top: 25px;
+  font-family: cursive, sans-serif;
+  position: absolute;
+  margin-top: 5px;
+}
+
+.btn-icon {
+  padding: 0;
+  border: none;
+  background: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0.8;
+  z-index: 1;
+  outline: none;
+}
+
+._icon {
+  font-size: 30px;
+  color: rgb(230, 230, 230, 2);
+}
+
+.items-count-notification {
+  border-radius: 50%;
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  background: rgb(247, 73, 125);
+  font-size: 15px;
+  font-weight: bold;
+  text-align: center;
+  font-size: 7px;
+  padding-top: 8px;
+  z-index: 2;
+  top: -12px;
+  left: 15px;
+}
+
+@media (max-width: 1150) {
+  .items-count-notification {
+    right: 70px;
   }
+}
 
-  a {
-    font-size: 30px;
+@media (max-width: 1000) {
+  .items-count-notification {
+    right: 100px;
   }
+}
 
-  .bar-right-content {
-    display: flex;
-    justify-content: space-evenly;
-    width: 100%;
+@media (max-width: 900) {
+  .items-count-notification {
+    right: 110px;
   }
+}
 
+@media (max-width: 1150) {
+  .items-count-notification {
+    right: 90px;
+  }
+}
+
+@media (max-width: 850px) {
   .nav-icon {
-    flex-basis: 15%;
-    text-align: end;
-    padding-top: 10px;
-    max-width: 50px;
-    position: relative;
-    z-index: 0;
+    text-align: center;
   }
 
   .desciption {
-    color: white;
-    font-size: 10px;
-    z-index: 2;
-    top: 25px;
-    font-family: cursive, sans-serif;
-    position: absolute;
-    margin-top: 5px;
-  }
-
-  .btn-icon {
-    padding: 0;
-    border: none;
-    background: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0.8;
-    z-index: 1;
-    outline: none;
-  }
-
-  ._icon {
-    font-size: 30px;
-    color: rgb(230, 230, 230, 2)
-  }
-
-  .items-count-notification {
-    border-radius: 50%;
-    position: absolute;
-    width: 25px;
-    height: 25px;
-    background: rgb(247, 73, 125);
-    font-size: 15px;
-    font-weight: bold;
     text-align: center;
-    font-size: 7px;
-    padding-top: 8px;
-    z-index: 2;
-    top: -12px;
-    left: 15px;
   }
 
-  @media (max-width: 1150) {
-    .items-count-notification {
-      right: 70px;
-    }
+  .bar-right-content {
+    width: 80%;
+    justify-content: space-around;
+  }
+}
 
+@media (max-width: 650px) {
+  .nav-icon {
+    text-align: center;
+    margin: 5px;
+    margin-bottom: 10px;
   }
 
-  @media (max-width: 1000) {
-    .items-count-notification {
-      right: 100px;
-    }
-
+  .desciption {
+    text-align: center;
   }
 
-  @media (max-width: 900) {
-    .items-count-notification {
-      right: 110px;
-    }
-
+  .bar-right-content {
+    width: 100%;
   }
 
-  @media (max-width: 1150) {
-    .items-count-notification {
-      right: 90px;
-    }
-
+  .favorites {
+    display: none;
   }
-
-  @media (max-width: 850px) {
-    .nav-icon {
-      text-align: center;
-    }
-
-    .desciption {
-      text-align: center;
-    }
-
-    .bar-right-content {
-      width: 80%;
-      justify-content: space-around;
-    }
-
-  }
-
-  @media (max-width: 650px) {
-    .nav-icon {
-      text-align: center;
-      margin: 5px;
-      margin-bottom: 10px;
-    }
-
-    .desciption {
-      text-align: center;
-    }
-
-    .bar-right-content {
-      width: 100%;
-    }
-
-    .favorites {
-      display: none;
-    }
-
-  }
+}
 </style>
